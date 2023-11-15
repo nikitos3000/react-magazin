35fecdd3d8eecc98efa6d9106046ae7a0b370fa1
nikita <trinevnikita@gmail.com>
2023-11-15 11:48:37 +0300

    b

--- src/App.js
+++ src/App.js
@@ -1,25 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="wrap clear">
+      <header className="d-flex justify-between align-center p-40">
+      <div className="d-flex align-center">
+      <img width={40} height={40}  src="/img/logo.jpg" />
+        <div>
+          <h3 className="text-uppercase">magazin</h3>
+          <p className="opacity-5">Это хороший магазин</p>
+        </div>
+      </div>
+        <ul className="d-flex">
+          <li className="mr-30">
+          <img width={18} height={18}  src="/img/cart.png" />
+            <span>
+            1000 руб.
+            </span>
+            </li>
+          <li>
+          <img width={18} height={18}  src="/img/user.jng" />
+            </li>
+        </ul>
       </header>
+      <div className="content p-40">
+      <h1 className="mb-40">Все товары</h1>
+<div className="d-flex">
+<div className="card">
+      <img width={133} height={112} src="" alt="" />
+      <h5>классный товар 1</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>10000</b>
+          <button className="button">
+            <img width={11} height={11} src="" alt=""></img>
+          </button>
+        </div>
+      </div>
+      </div>
+      <div className="card">
+      <img width={133} height={112} src="" alt="" />
+      <h5>классный товар 1</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>10000</b>
+          <button className="button">
+            <img width={11} height={11} src="" alt=""></img>
+          </button>
+        </div>
+      </div>
+      </div>
+      <div className="card">
+      <img width={133} height={112} src="" alt="" />
+      <h5>классный товар 1</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>10000</b>
+          <button className="button">
+            <img width={11} height={11} src="" alt=""></img>
+          </button>
+        </div>
+      </div>
+      </div>
+      <div className="card">
+      <img width={133} height={112} src="" alt="" />
+      <h5>классный товар 1</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>10000</b>
+          <button className="button">
+            <img width={11} height={11} src="" alt=""></img>
+          </button>
+        </div>
+      </div>
+      </div>
+      <div className="card">
+      <img width={133} height={112} src="" alt="" />
+      <h5>классный товар 1</h5>
+      <div className="d-flex justify-between align-center">
+        <div className="d-flex flex-column">
+          <span>Цена:</span>
+          <b>10000</b>
+          <button className="button">
+            <img width={11} height={11} src="" alt=""></img>
+          </button>
+        </div>
+      </div>
+      </div>
+      
+</div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
